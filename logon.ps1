ec69af3005e0bf2966368d160580057da64ba5b0
Evgeniy Tkachenko <et.coder@gmail.com>
2023-10-13 22:22:39 +0300

    feat: Added time to load audio file

--- logon.ps1
+++ logon.ps1
@@ -3,6 +3,16 @@ if (![System.IO.File]::Exists('Sound_10460.mp3')) {
 }
 
 Add-Type -AssemblyName presentationCore
+
 $Player = New-Object system.windows.media.mediaplayer
 $Player.open($PSScriptRoot + '\Sound_10460.mp3')
-$Player.play()
+
+Start-Sleep 1 # allows time to load the audio file
+$duration = $Player.NaturalDuration.TimeSpan.TotalSeconds
+$Player.Play()
+Start-Sleep $duration
+
+$Player.Stop()
+$Player.Close()
+
+exit
